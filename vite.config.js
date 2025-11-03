1755d8e3f3d335948c8969268bc7914a5ad2a1d3
Beverly140562 <beverlysalde.140562@gmail.com>
2025-11-03 21:41:50 +0800

    Fix icons

--- vite.config.js
+++ vite.config.js
@@ -15,8 +15,8 @@ export default defineConfig({
         background_color: '#ffffff',
         theme_color: '#2563eb',
         icons: [
-          { src: '/logo192.jpg', sizes: '192x192', type: 'image/jpg' },
-          { src: '/logo512.png', sizes: '512x512', type: 'image/png' }
+          { src: '/pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/pwa-512x512.png', sizes: '512x512', type: 'image/png' }
         ]
       }
     })
