9e87e26b74a89bb165ee24313b8ef083d7507309
Beverly140562 <beverlysalde.140562@gmail.com>
2025-11-03 20:50:25 +0800

    Initial PWA commit

---
+++ dist/service-worker.js
@@ -0,0 +1,18 @@
+
+// public/service-worker.js
+
+self.addEventListener('install', (event) => {
+  console.log('🛠️ Service Worker: Installed');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('🚀 Service Worker: Activated');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
